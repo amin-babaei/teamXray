888b018ace8eba14dfc2d7edcc4d54f3983dbcbe
amin <aminbabaei82@gmail.com>
2023-04-04 14:32:23 +0430

    fix footer home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Twitch from '../components/twitch/Twitch'
 import Players from '../components/players/Players';
 import Blog from '../components/blog/Blog';
 import ContactUs from '../components/ContactUs';
-import Footer from '../components/common/Footer';
 import Partners from './../components/Partners';
 import Mobile from './../components/menu/Mobile';
 import { Helmet } from 'react-helmet-async';
@@ -12,22 +11,20 @@ import { Helmet } from 'react-helmet-async';
 const Home = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <meta charSet="utf-8" />
         <title>xrayTeam</title>
-    </Helmet>
-    <Header/>
-    <Mobile/>
-    <main className='font-main'>
-      <Twitch/>
-      <Players/>
-      <Blog/>
-      <ContactUs/>
-      <Partners/>
-    </main>
-    <Footer/>
+      </Helmet>
+      <Header />
+      <Mobile />
+      <main className='font-main'>
+        <Twitch />
+        <Players />
+        <Blog />
+        <ContactUs />
+        <Partners />
+      </main>
     </>
-   
   )
 }
 

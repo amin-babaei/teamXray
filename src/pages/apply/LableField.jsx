394dd717dcde785af9b2341bf4e22f2b67f2d698
amin <aminbabaei82@gmail.com>
2023-07-19 16:26:41 +0330

    style: change position style experiences input and created arrow for error message

--- src/pages/apply/LableField.jsx
+++ src/pages/apply/LableField.jsx
@@ -1,7 +1,7 @@
 import { ErrorMessage, useField } from "formik"
 
 const LableField = ({ name, lable }) => {
-  const [, meta] = useField(name);
+  const [field, meta] = useField(name);
   const error = meta.touched && meta.error;
   return (
     <>
@@ -13,9 +13,9 @@ const LableField = ({ name, lable }) => {
       </label>
       <ErrorMessage
         name={name}
-        render={(msg) => <div className="absolute bg-xred p-2 -top-4 text-white font-bold text-sm">
+        render={(msg) => <div className={`absolute bg-xred p-2 ${field.name === "experiences" ? "top-1" : "-top-4"} text-white font-bold text-sm`}>
           {msg}
-          <div className='h-1 w-full bg-black absolute top-8 left-3'></div>
+          <div className='h-3 w-3 rotate-45 bg-xred absolute top-7 left-2'></div>
         </div>}
       />
     </>

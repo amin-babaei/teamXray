96752897036cc5f21074551ff63f9260b63a4d57
amin <aminbabaei82@gmail.com>
2023-08-12 12:56:05 +0330

    fix: fix loading ui display

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,60 +29,61 @@ const Login =() => {
         <meta name="description" content="login page in xray team" />
       </Helmet>
     <div className="containerr py-14">
-        {loading && <Loading/>}
-        <section className="flex justify-center items-center ">
-          <Formik
-          initialValues={{
-            email: '',
-            password: ''
-          }}
-          validationSchema={loginSchema}
-          onSubmit={(values) => {
-            handleSubmit(values)
-          }}>
-            <Form className="bg-black rounded-lg border border-white w-full sm:w-3/5 px-5 sm:px-16 py-10" >
-                <Link to={"/"} className="w-40 block mb-10 mx-auto">
-                    <img src="/images/logo.svg" alt="xrayteam"/>
-                </Link>
-            <div className="flex items-center mb-10 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-            <p className="text-center text-3xl font-main mx-4 mb-0 text-xred">login</p>
-          </div>
-          <ErrorMessage
-                          name="email"
-                          render={(msg) => (
-                            <div className="text-xred">{msg}</div>
-                          )}
-                        />
-              <Field
-                type="email"
-                name='email'
-                className="input focus:border-xred focus:border-b-4"
-                placeholder="Email"
-                autoComplete= "email"
-              />
-                <ErrorMessage
-                            name="password"
+        {loading ? <Loading/> : (
+          <section className="flex justify-center items-center ">
+            <Formik
+            initialValues={{
+              email: '',
+              password: ''
+            }}
+            validationSchema={loginSchema}
+            onSubmit={(values) => {
+              handleSubmit(values)
+            }}>
+              <Form className="bg-black rounded-lg border border-white w-full sm:w-3/5 px-5 sm:px-16 py-10" >
+                  <Link to={"/"} className="w-40 block mb-10 mx-auto">
+                      <img src="/images/logo.svg" alt="xrayteam"/>
+                  </Link>
+              <div className="flex items-center mb-10 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+              <p className="text-center text-3xl font-main mx-4 mb-0 text-xred">login</p>
+            </div>
+            <ErrorMessage
+                            name="email"
                             render={(msg) => (
                               <div className="text-xred">{msg}</div>
                             )}
                           />
-              <Field
-                type="password"
-                name='password'
-                className="input focus:border-xred focus:border-b-4"
-                placeholder="Password"
-                autoComplete= "current-password"
-              />
-              <button
-                type="submit"
-                className="btn w-full focus:outline-none"
-              >
-                Submit
-              </button>
-              <Social/>
-            </Form>
-          </Formik>
-        </section>
+                <Field
+                  type="email"
+                  name='email'
+                  className="input focus:border-xred focus:border-b-4"
+                  placeholder="Email"
+                  autoComplete= "email"
+                />
+                  <ErrorMessage
+                              name="password"
+                              render={(msg) => (
+                                <div className="text-xred">{msg}</div>
+                              )}
+                            />
+                <Field
+                  type="password"
+                  name='password'
+                  className="input focus:border-xred focus:border-b-4"
+                  placeholder="Password"
+                  autoComplete= "current-password"
+                />
+                <button
+                  type="submit"
+                  className="btn w-full focus:outline-none"
+                >
+                  Submit
+                </button>
+                <Social/>
+              </Form>
+            </Formik>
+          </section>
+        )}
       </div>
     </main>
   )

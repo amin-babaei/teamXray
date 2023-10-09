75602ba35380586c8fa74a99234fe45137331381
amin <aminbabaei82@gmail.com>
2023-10-09 18:18:47 +0330

    fix: I forgot to secure 😐

--- src/helpers/SecureRoute.jsx
+++ src/helpers/SecureRoute.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Loading from './Loading';
+import NotFound from '../pages/NotFound';
+import { useEffect } from 'react';
+import { userProfile } from '../app/features/user/userAction';
 
 export const SecureRoute = () => {
-    const { loading } = useSelector((state) => state.user)
+    const { loading, userInfo } = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(userProfile())
+    },[dispatch])
+
     if(loading) return <Loading/>
-    
-    return <Outlet/>;
+    return userInfo ? <Outlet/> : <NotFound/>;
 }
